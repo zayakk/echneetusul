3fa9bf7ad472a6ee541b08ee9754877af6ece940
GanzorigUlziibat <94421033+GanzorigUlziibat@users.noreply.github.com>
2025-03-22 18:11:21 +0800

    tailbar nemlee

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from benglish import svTsag
 
 urlpatterns = [
-    path('tsag/', svTsag.dt_time),
+    path('tsag/', svTsag.dt_time), # localhost:8000/tsag/ gehed svTsag.dt_timecfunction duudna.
 ]
--- backend/benglish/svTsag.py
+++ backend/benglish/svTsag.py
@@ -3,6 +3,6 @@ from datetime import datetime
 
 
 def dt_time(request): #{key : value, key1 : value1, key2 : value2, }
-    respData = [{"tsag":str(datetime.now())}]
-    resp = {"action":"time","data":respData, "size":1,  "resultCode":"200", "resultMessage":"Success", "datetimes":datetime.now()}
-    return (JsonResponse (resp))
+    respData = [{"tsag":str(datetime.now())}] # response-n data-g beldej baina. data key ni list baih buguud list dotor dictionary baina.
+    resp = {"action":"time","data":respData, "size":1,  "resultCode":"200", "resultMessage":"Success", "curdate":datetime.now()} # response beldej baina. 6 keytei
+    return (JsonResponse (resp)) # response bustsaaj baina
