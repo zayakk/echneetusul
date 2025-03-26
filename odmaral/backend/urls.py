449d68fe7419f48aef78f4682578536763a46596
Odmaral <lnodmaral@gmail.com>
2025-03-26 10:04:30 +0800

    time zone

--- odmaral/backend/urls.py
+++ odmaral/backend/urls.py
@@ -19,5 +19,6 @@ from django.urls import path
 from benglish import svTsag
 
 urlpatterns = [
-    path('tsag/', svTsag.dt_time),
+    path('tsag/', svTsag.checkService),
+    # path('tsagUB/', svTsag.dt_time_ub)
 ]
--- odmaral/benglish/svTsag.py
+++ odmaral/benglish/svTsag.py
@@ -1,8 +1,29 @@
 from django.http.response import JsonResponse
 from datetime import datetime
+import pytz, json
 
 def dt_time(request):
     restData = [{"time":str(datetime.now())}]
     resp = {"action":"time", "data":restData, "size":1, 'resultCode':'200',
                           'resultMessage':'Success', "datetime":datetime.now()}
-    return (JsonResponse(resp))
+    return (resp)
+
+def dt_time_ub(request):
+    ub_tz=pytz.timezone("Asia/Ulaanbaatar")
+
+    ub_time = datetime.now(ub_tz)
+    restData = [{"time":ub_time.strftime("%Y-%m-%d %H:%M:%S")}]
+    resp = {"action":"time", "data":restData, "size":1, 'resultCode':'200',
+                          'resultMessage':'Success', "datetime":datetime.now()}
+    return (resp)
+
+def checkService(request):
+    jsons = json.loads(request.body)
+    action = jsons ['action']
+    if action == "time":
+        result = dt_time(request)
+        return JsonResponse(result)
+    elif action == "time_ub":
+        result = dt_time_ub(request)
+        return JsonResponse(result)
+    return(JsonResponse({}))
