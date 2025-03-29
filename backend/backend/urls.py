473b31cc7ac2a2cde249cf16e82ea4b72b7ee220
Odmaral <lnodmaral@gmail.com>
2025-03-29 19:09:46 +0800

    Update urls.py

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,11 +1,11 @@
 
 from django.urls import path
-from benglish import svTsag, svUser
+from benglish import svTsag, svUser, svKnow, svUnknow,
 
 urlpatterns = [
     path('tsag/', svTsag.checkService), # localhost:8000/tsag/ gehed svTsag.checkService function duudna.
     # path('tsagub/', svTsag.dt_timeub), # localhost:8000/tsag/ gehed svTsag.dt_time function duudna.
     path('user/', svUser.checkService),
-    # path('uwknow/', svKnow.checkService),
-    # path('uwunknow/', svUnKnow.checkService),
+    path('know/', svKnow.checkService),
+    path('uwunknow/', svUnknow.checkService),
 ]
