d44a2adaeaf020e7695ef5c7413aed485309ab4b
GanzorigUlziibat <94421033+GanzorigUlziibat@users.noreply.github.com>
2025-03-29 19:12:14 +0800

    urls

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('tsag/', svTsag.checkService), # localhost:8000/tsag/ gehed svTsag.checkService function duudna.
     # path('tsagub/', svTsag.dt_timeub), # localhost:8000/tsag/ gehed svTsag.dt_time function duudna.
     path('user/', svUser.checkService),
+    # comment
     path('know/', svKnow.checkService),
     path('uwunknow/', svUnknow.checkService),
 ]
