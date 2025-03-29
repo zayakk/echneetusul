af0973c5db653faad241b4b3474c399ce73fda00
Urnukh18 <uunuu8271@gmail.com>
2025-03-29 14:43:32 +0800

    tsag service

---
+++ Urnukh/backend/benglish/svTsag.py
@@ -0,0 +1,6 @@
+from django.http.response import JsonResponse
+from datetime import datetime
+
+def dt_time(request):
+    data = [{"time":datetime.now()}]
+    return (JsonResponse({"action":"tsag", "data":data, "resultCode":200, "resultMessage":"Success", "size":len(data), "curdate":datetime.now()}))
