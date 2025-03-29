4e949f457978438bfa9df23c1fe95c96542e1100
Anar0109 <anar.tuvshinjargal@gmail.com>
2025-03-29 14:37:05 +0800

    day2

--- anar/backend/benglish/svtsag.py
+++ anar/backend/benglish/svtsag.py
@@ -1,2 +1,6 @@
+from django.http.response import JsonResponse
+from datetime import datetime
+
 def dt_time(request):
-    return ("Hello World")
+    data=[{"time":datetime.now()}]
+    return (JsonResponse({"action":"tsag", "data":data, "resultCode":200, "resultMessage":"Success", "size":len(data), "curdate":datetime.now()}))
